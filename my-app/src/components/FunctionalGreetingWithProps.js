e4aa8b9f6d32214cd71a3ff4117bb345ffcacea9
RoshnaVakkeel <vakkeel.roshna@gmail.com>
2023-01-28 21:19:21 +0000

    Functional components and props

--- my-app/src/components/FunctionalGreetingWithProps.js
+++ my-app/src/components/FunctionalGreetingWithProps.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-//function FunctionalGreeting(){
-//    return <h1>Hello from React!</h1>
-//}
+const FunctionalGreetingWithProps = (props) => {
+console.log(props);
 
-const FunctionalGreetingWithProps = () => <h1>Hello!!!</h1>
+return <h1>Hello {props.name}, I see that you are {props.age} years old. {props.greeting}!!!</h1>;
+}
 
-export default FunctionalGreetingWithProps
+
+export default FunctionalGreetingWithProps;
