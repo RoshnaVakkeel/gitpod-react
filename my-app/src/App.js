6257a6af7236616ddffc78aced29b0362408ced4
RoshnaVakkeel <vakkeel.roshna@gmail.com>
2023-01-30 11:01:56 +0000

    Conditional Rendering, Methods as Prop Parents

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,32 +1,36 @@
 import './App.css';
-import FunctionalGreeting from './components/FunctionalGreeting';
-import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
-import StatefulGreeting from './components/StatefulGreeting';
-import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
-import StatefulGreetingWIthPrevState from './components/StatefulGreetingWIthPrevState';
-import StatefulGreetingWithPrevStateWithIncrement from './components/StatefulGreetingWIthPrevStateWithIncrement';
-import StatefulGreetingWithPrevStateWithIncrementFix from './components/StatefulGreetingWIthPrevStateWithIncrement';
-import EventsFunctional from './components/EventsFunctional';
-import EventsClass from './components/EventsClass';
-import EventBinding from './components/EventBinding'
-import ConditionalRenderingClass from './components/ConditionalRendering';
+// import FunctionalGreeting from './components/FunctionalGreeting';
+// import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+// import StatefulGreeting from './components/StatefulGreeting';
+// import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
+// import StatefulGreetingWIthPrevState from './components/StatefulGreetingWIthPrevState';
+// import StatefulGreetingWithPrevStateWithIncrement from './components/StatefulGreetingWIthPrevStateWithIncrement';
+// import StatefulGreetingWithPrevStateWithIncrementFix from './components/StatefulGreetingWIthPrevStateWithIncrement';
+// import EventsFunctional from './components/EventsFunctional';
+// import EventsClass from './components/EventsClass';
+// import EventBinding from './components/EventBinding'
+// import ConditionalRenderingClass from './components/ConditionalRendering';
 import NestingComponents from './components/NestingComponents'; 
+import MethodsAsPropsParent from './components/MethodsAsPropsParent';
+
+
 
 function App() {
   return (
     <div className="App">
-      <ConditionalRenderingClass />
+      {/* <ConditionalRenderingClass /> */}
+      <MethodsAsPropsParent />
       <NestingComponents />
-      <EventsFunctional />
-      <EventsClass />
-      <EventBinding />
-      <FunctionalGreeting />
-      <FunctionalGreetingWithProps greeting= "Nice to meet you!" name = "Mike" age= "32"/>
+      {/* <EventsFunctional />
+      <EventsClass /> */}
+      {/* <EventBinding />
+      <FunctionalGreeting /> */}
+      {/* <FunctionalGreetingWithProps greeting= "Nice to meet you!" name = "Mike" age= "32"/>
       <StatefulGreeting greeting="I'm a stateful class component!" name="Mike"/>
       <StatefulGreetingWithCallback greeting="I'm a stateful class component with callback function!" name="Mike"/>
       <StatefulGreetingWIthPrevState greeting="I'm a stateful class component with callback function and previous value return!" name="Mike"/>
-      <StatefulGreetingWithPrevStateWithIncrement greeting="I'm a stateful class component with callback function with increment!" name="Mike"/> */}
-      <StatefulGreetingWithPrevStateWithIncrementFix greeting="I'm a stateful class component with callback function with increment Fix!" name="Mike"/>
+      <StatefulGreetingWithPrevStateWithIncrement greeting="I'm a stateful class component with callback function with increment!" name="Mike"/> */} */}
+      {/* <StatefulGreetingWithPrevStateWithIncrementFix greeting="I'm a stateful class component with callback function with increment Fix!" name="Mike"/> */}
     </div>
   );
 };
